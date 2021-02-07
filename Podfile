af5c5f601a28ebe50bc543c2d482c111d868e023
MartinMugiMai <mugimaimai@users.noreply.github.com>
2021-02-07 21:24:31 +0800

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+project 'yuyintest2.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'yuyintest2' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Weather
+  
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  pod 'ProgressHUD'
+  
+ 
+end
---
+++ yuyintest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yuyintest2
+//
+//  Created by 麦俊杰 on 2021/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func yyb(_ sender: Any) {
+        print("语音识别按钮")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
